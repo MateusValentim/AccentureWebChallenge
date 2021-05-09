dd61b60d000381f7201c9279e12c54ef3ae08f2c
MateusValentim <mateus.valentim19@hotmail.com>
2021-05-09 12:43:24 -0300

    Criação do pacote testDataType, classe responsavel por manter todas as classes de tipos de dados apenas neste pacote

---
+++ src/main/java/testDataType/CustomerData.java
@@ -0,0 +1,14 @@
+package testDataType;
+
+public class CustomerData {
+	public String make;
+	public String enginePerformanceKw;
+	public String inputCalendarDate;
+	public String numberOfSeats;
+	public String fuelType;
+	public String inputListPrice;
+	public String inputLicensePlateNumber;
+	public String inputAnnualMileage;
+
+
+}
